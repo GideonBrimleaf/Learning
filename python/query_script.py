5a3617f47cd8a907df21d3f767dc060804eddd57
GideonBrimleaf <chrisdryden1@gmail.com>
2018-03-02 21:59:23 +0000

    forgot to add my sql script lol

--- python/query_script.py
+++ python/query_script.py
@@ -4,7 +4,7 @@ from connection_script import SqlConnection
 
 def main(filename):
     conn = SqlConnection().mdbdw_connection()
-    file = open(filename, "r+")
+    file = open(filename, "r")
     query_string = file.read()
     results_table = pd.read_sql(query_string,conn)
     print(results_table.head())
