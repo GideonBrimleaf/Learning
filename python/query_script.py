6026c7ac595d9f35d4fe1f590d2fb0db6b518b87
GideonBrimleaf <chrisdryden1@gmail.com>
2018-03-02 21:25:43 +0000

    script now takes in a sql string argument

--- python/query_script.py
+++ python/query_script.py
@@ -1,11 +1,10 @@
 import pandas as pd
 from connection_script import SqlConnection
 
-def main():
+def main(query_string):
     conn = SqlConnection().mdbdw_connection()
-    query_string = "SELECT TOP 10 * FROM DWInternal.FactTransaction"
     results_table = pd.read_sql(query_string,conn)
     print(results_table.head())
 
 if __name__ == '__main__':
-    main()
+    main("SELECT TOP 10 * FROM DWInternal.FactTransaction")
