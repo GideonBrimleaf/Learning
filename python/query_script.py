c93d9c1816197f04a88f7c39cfc5550d93d508bf
GideonBrimleaf <chrisdryden1@gmail.com>
2018-03-02 21:54:12 +0000

    query script now reads in a file yay!

--- python/query_script.py
+++ python/query_script.py
@@ -2,8 +2,13 @@ import sys
 import pandas as pd
 from connection_script import SqlConnection
 
-def main(query_string):
+#file = open("sql_string.sql", "r") or r+ - not sure yet
+#sql string = file.read()
+
+def main(filename):
     conn = SqlConnection().mdbdw_connection()
+    file = open(filename, "r+")
+    query_string = file.read()
     results_table = pd.read_sql(query_string,conn)
     print(results_table.head())
 
