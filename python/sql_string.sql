d290e1005e6480ebb396814994f7f3779f5f0cd5
GideonBrimleaf <chrisdryden1@gmail.com>
2018-03-02 22:10:00 +0000

    Slightly more pimped up sql just for testing

--- python/sql_string.sql
+++ python/sql_string.sql
@@ -1,1 +1,12 @@
-SELECT TOP 10 * FROM DWInternal.FactTransaction
+WITH cte AS 
+(SELECT dimUserId
+FROM DWInternal.DimUser
+WHERE age BETWEEN 25 AND 35 
+)
+
+SELECT COUNT(DISTINCT cte.dimUserId) as userCount,
+       SUM(debitAmount) as sumOfSpend,
+       COUNT(factTransactionId) as transactionCount
+FROM DWInternal.FactTransaction
+INNER JOIN cte ON 
+DWInternal.FactTransaction.dimUserId = cte.dimUserId
