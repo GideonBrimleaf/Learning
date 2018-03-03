53b942764fd81e022fd44145aab0b24fce35e714
GideonBrimleaf <chrisdryden1@gmail.com>
2018-03-03 11:47:38 +0000

    refactored code to split out reading from executing

--- python/query_script.py
+++ python/query_script.py
@@ -2,12 +2,20 @@ import sys
 import pandas as pd
 from connection_script import SqlConnection
 
-def main(filename):
-    conn = SqlConnection().mdbdw_connection()
+def read_sql_query(filename):
     file = open(filename, "r")
     query_string = file.read()
+    return query_string
+
+def execute_sql_query(query_string):
+    conn = SqlConnection().mdbdw_connection()
     results_table = pd.read_sql(query_string,conn)
     return(results_table.head())
 
+def main():
+    sql_query = read_sql_query(sys.argv[1])
+    sql_results = execute_sql_query(sql_query)
+    return sql_results
+
 if __name__ == '__main__':
-    print(main(sys.argv[1]))
+    print(main())
