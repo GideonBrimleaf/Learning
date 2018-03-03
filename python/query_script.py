88d41a318fe217f88c228e250cde6a17f32f097d
GideonBrimleaf <chrisdryden1@gmail.com>
2018-03-03 21:58:48 +0000

    script now toggles between bar or line charts

--- python/query_script.py
+++ python/query_script.py
@@ -14,27 +14,30 @@ def execute_sql_query(query_string):
     results_table = pd.read_sql(query_string,conn)
     return results_table
 
-def plot_results(dataframe, column_name):
+def plot_results(dataframe, column_name, chart_type = "line"):
     fig, ax = plt.subplots()
     first_column = dataframe.iloc[:,0]
     x_axis = np.arange(len(first_column))
     x_axis_labels = first_column
     x_axis_name = dataframe.dtypes.index[0]
-    ax.plot(x_axis, dataframe[column_name])
+    if chart_type == "line":
+        ax.plot(x_axis, dataframe[column_name])
+    else:
+        ax.bar(x_axis, dataframe[column_name])
     plt.xticks(x_axis, x_axis_labels, rotation = 45)
     ax.set(xlabel = x_axis_name, title = column_name)
     return plt.show()
 
-def generate_graphs(dataframe):
+def generate_graphs(dataframe, chart_type = "line"):
     for column_name in dataframe.dtypes.index[1:]:
-        graph = plot_results(dataframe, column_name)
+        graph = plot_results(dataframe, column_name, chart_type)
     return graph
 
-def main(sql_file):
+def main(sql_file, chart_type = "line"):
     sql_query = read_sql_query(sql_file)
     sql_results = execute_sql_query(sql_query)
-    graphs = generate_graphs(sql_results)
+    graphs = generate_graphs(sql_results, chart_type)
     return graphs
 
-if __name__ == '__main__':
-    main(sys.argv[1])
+ if __name__ == '__main__':
+    main(sys.argv[1], sys.argv[2])
