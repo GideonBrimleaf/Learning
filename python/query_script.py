ee60a74404816fcf29dce5d8252310af8b7729b1
GideonBrimleaf <chrisdryden1@gmail.com>
2018-03-03 11:35:48 +0000

    query script returns rather than prints

--- python/query_script.py
+++ python/query_script.py
@@ -7,7 +7,7 @@ def main(filename):
     file = open(filename, "r")
     query_string = file.read()
     results_table = pd.read_sql(query_string,conn)
-    print(results_table.head())
+    return(results_table.head())
 
 if __name__ == '__main__':
-    main(sys.argv[1])
+    print(main(sys.argv[1]))
