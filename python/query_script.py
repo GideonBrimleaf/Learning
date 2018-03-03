a56263903b82728e50e36e6f04845c2b81b3a22f
GideonBrimleaf <chrisdryden1@gmail.com>
2018-03-03 13:04:51 +0000

    results can now be plotted

--- python/query_script.py
+++ python/query_script.py
@@ -1,6 +1,8 @@
 import sys
 import pandas as pd
 from connection_script import SqlConnection
+import matplotlib.pyplot as plt
+import numpy as np
 
 def read_sql_query(filename):
     file = open(filename, "r")
@@ -12,10 +14,21 @@ def execute_sql_query(query_string):
     results_table = pd.read_sql(query_string,conn)
     return(results_table.head())
 
+sql_query = read_sql_query("sql_string.sql")
+sql_results = execute_sql_query(sql_query)
+
+def plot_results(dataframe):
+    fig, ax = plt.subplots()
+    first_column = dataframe.iloc[:,0]
+    x_axis = np.arange(len(first_column))
+    ax.plot(x_axis, dataframe.userCount)
+    plt.show()
+
 def main():
     sql_query = read_sql_query(sys.argv[1])
     sql_results = execute_sql_query(sql_query)
-    return sql_results
+    graph = plot_results(sql_results)
+    return graph
 
 if __name__ == '__main__':
-    print(main())
+    main()
--- python/sql_string.sql
+++ python/sql_string.sql
@@ -4,9 +4,16 @@ FROM DWInternal.DimUser
 WHERE age BETWEEN 25 AND 35 
 )
 
-SELECT COUNT(DISTINCT cte.dimUserId) as userCount,
+SELECT calendarYearMonth,
+       COUNT(DISTINCT cte.dimUserId) as userCount,
        SUM(debitAmount) as sumOfSpend,
        COUNT(factTransactionId) as transactionCount
 FROM DWInternal.FactTransaction
 INNER JOIN cte ON 
 DWInternal.FactTransaction.dimUserId = cte.dimUserId
+INNER JOIN DWInternal.DimDate
+ON DWInternal.DimDate.dimDateId = 
+DWInternal.FactTransaction.dimTransactionDateId
+WHERE calendarYearMonth > '201708'
+GROUP BY calendarYearMonth
+ORDER BY calendarYearMonth
