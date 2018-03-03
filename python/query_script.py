600edfedc60e1eef982df2be6c9001909abde41a
GideonBrimleaf <chrisdryden1@gmail.com>
2018-03-03 14:19:31 +0000

    Generic plot_graph function

--- python/query_script.py
+++ python/query_script.py
@@ -14,21 +14,21 @@ def execute_sql_query(query_string):
     results_table = pd.read_sql(query_string,conn)
     return(results_table.head())
 
-sql_query = read_sql_query("sql_string.sql")
-sql_results = execute_sql_query(sql_query)
-
-def plot_results(dataframe):
+def plot_results(dataframe, column_name):
     fig, ax = plt.subplots()
     first_column = dataframe.iloc[:,0]
     x_axis = np.arange(len(first_column))
-    ax.plot(x_axis, dataframe.userCount)
-    plt.show()
+    ax.plot(x_axis, dataframe[column_name])
+    return plt.show()
 
 def main():
     sql_query = read_sql_query(sys.argv[1])
     sql_results = execute_sql_query(sql_query)
-    graph = plot_results(sql_results)
+    graph = plot_results(sql_results, 'userCount')
     return graph
+
+# sql_query = read_sql_query("sql_string.sql")
+# sql_results = execute_sql_query(sql_query)
 
 if __name__ == '__main__':
     main()
