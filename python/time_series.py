04de43bf0859692eaff59745461afb2b4de0c3a2
GideonBrimleaf <chrisdryden1@gmail.com>
2018-03-27 22:16:31 +0100

    stationarity introduced

--- python/time_series.py
+++ python/time_series.py
@@ -1,12 +1,46 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pylab as plt
-from matplotlib.pylab import rcParams
-rcParams['figure.figsize'] = 15, 6
+from statsmodels.tsa.stattools import adfuller
+# from matplotlib.pylab import rcParams
+# rcParams['figure.figsize'] = 15, 6
 
 dateparse = lambda dates: pd.datetime.strptime(dates, '%Y-%m')
 data = pd.read_csv('passengers.csv', parse_dates=['Month'], index_col=['Month'],date_parser=dateparse)
 data.index
 
 ts = data['PassengerCount']
-ts.head(10)
+
+#Dickey Fuller to measure stationarity
+def test_stationarity(timeseries):
+    rolmean = timeseries.rolling(window=12,center=False).mean()
+    rolstd = timeseries.rolling(window=12,center=False).std()
+    #Perform Dickey-Fuller test:
+    print('Results of Dickey-Fuller Test:')
+    dftest = adfuller(timeseries, autolag='AIC')
+    dfoutput = pd.Series(dftest[0:4], index=['Test Statistic','p-value','#Lags Used','Number of Observations Used'])
+    for key,value in dftest[4].items():
+        dfoutput['Critical Value (%s)'%key] = value
+    print(dfoutput)
+        #Plot rolling statistics:
+    plt.plot(timeseries, color='blue',label='Original')
+    plt.plot(rolmean, color='red', label='Rolling Mean')
+    plt.plot(rolstd, color='black', label = 'Rolling Std')
+    plt.legend()
+    #plt.title('Rolling Mean & Standard Deviation')
+    plt.show()
+
+#let's start removing the trend and making the data more stationary
+ts_log = np.log(ts)
+moving_avg = ts_log.rolling(window=12,center=False).mean()
+plt.plot(ts_log)
+plt.plot(moving_avg, color='red')
+plt.show()
+
+ts_log_moving_avg_diff = ts_log - moving_avg
+ts_log_moving_avg_diff.dropna(inplace=True)
+test_stationarity(ts_log_moving_avg_diff)
+
+ts_log_diff = ts_log - ts_log.shift()
+ts_log_diff.dropna(inplace=True)
+test_stationarity(ts_log_diff)
